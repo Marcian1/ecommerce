91fb3864f0fed667ad73b67b41ea161470db2822
Z0060883 <marcian.bondoc-popescu.external@trw.com>
2020-12-19 23:10:09 +0200

    Add tab group for course component

--- src/app/modules/courses/app.module.ts
+++ src/app/modules/courses/app.module.ts
@@ -1,5 +1,6 @@
 
 import { NgModule } from '@angular/core';
+import { MaterialModule } from 'src/app/material-ui.module';
 import { CoursesComponent } from './components/courses/courses.component';
 
 
@@ -7,7 +8,7 @@ import { CoursesComponent } from './components/courses/courses.component';
   declarations: [
   CoursesComponent],
   imports: [
-
+  MaterialModule
   ],
   providers: [],
   bootstrap: []
