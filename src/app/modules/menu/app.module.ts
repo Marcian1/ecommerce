0cd06ea8be551707d304c4a5103fefc45b96f378
Z0060883 <marcian.bondoc-popescu.external@trw.com>
2020-12-19 23:00:10 +0200

    Defining the routerlink

--- src/app/modules/menu/app.module.ts
+++ src/app/modules/menu/app.module.ts
@@ -1,5 +1,6 @@
 
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 import { MaterialModule } from 'src/app/material-ui.module';
 import { MenuTopComponent } from './components/menu-top/menu-top.component';
 
@@ -9,6 +10,7 @@ import { MenuTopComponent } from './components/menu-top/menu-top.component';
   MenuTopComponent],
   imports: [
     MaterialModule,
+    AppRoutingModule
   ],
   exports: [MenuTopComponent],
   providers: [],
