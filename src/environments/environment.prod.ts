b6ab8dd15c891560f61bd165abfbc102d4b0c25a
Z0060883 <marcian.bondoc-popescu.external@trw.com>
2020-12-22 21:25:54 +0200

    firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,13 @@
 export const environment = {
-  production: true, 
+  production: true,
   firebase :
   {
-    
+    apiKey: 'AIzaSyBw_U0fry9-XR8-jNLaz9Vi7hdk34K-gtY',
+    authDomain: 'project-firebase-70320.firebaseapp.com',
+    projectId: 'project-firebase-70320',
+    storageBucket: 'project-firebase-70320.appspot.com',
+    messagingSenderId: '456177360575',
+    appId: '1:456177360575:web:a102a47903e6564b27300a',
+    measurementId: 'G-S0JLZ1X34F'
   }
 };
